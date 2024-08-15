5caa7536f4120f1013bf7ec63c26c491a1a1726a
Yukari7777 <yys14239102@gmail.com>
2024-08-15 06:14:42 -0400

    v1.1.0
    - Added a shortcut key bind to switch the mod on/off in mod configuration, defaults by HOME.
    - Now properly works when lag compensation is on.
    - Fixed an issue it can target on object where it couldn't

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name = "Safe Telepoof"
-version = "1.0.2"
+version = "1.1.0"
 description = "Stop wasting your lazy explorer's durability!\n\nWhile holding lazy explorer:\n- Right click + Left Click to telepoof\n- Right Click + Right Click to cancel"
 author = "Yukari7777"
 api_version = 10
@@ -21,6 +21,10 @@ if folder_name:find("SafeTelepoof") then
     name = name.." - local"
 end
 
+local Keys = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "PERIOD", "SLASH", "SEMICOLON", "TILDE", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "INSERT", "DELETE", "HOME", "END", "PAGEUP", "PAGEDOWN", "MINUS", "EQUALS", "BACKSPACE", "CAPSLOCK", "SCROLLOCK", "BACKSLASH"}
+local KeyOptions = {}
+for i = 1, #Keys do KeyOptions[i] = { description = ""..Keys[i].."", data = "KEY_"..Keys[i] } end
+
 server_filter_tags = {
 	"interface",
 	"tweak",
@@ -31,4 +35,11 @@ server_filter_tags = {
 }
 
 configuration_options = {
+	{
+		name = "enabled_key",
+		label = "Turn on/off mod by",
+		hover = "Set which key to switch the mod on/off",
+		options = KeyOptions,
+		default = "KEY_HOME",
+	},
 }
--- modmain.lua
+++ modmain.lua
@@ -10,24 +10,42 @@ local UpvalueHacker = GLOBAL.require "tools/UpvalueHacker"
 
 STRINGS.ACTIONS.CASTAOE.ORANGESTAFF = STRINGS.ACTIONS.BLINK.GENERIC
 
+local mod_enabled = true
+local mod_enabled_key = GetModConfigData("enabled_key")
+TheInput:AddKeyDownHandler(GLOBAL[mod_enabled_key], function()
+    if GLOBAL.ThePlayer ~= nil and GLOBAL.ThePlayer.components.talker ~= nil then 
+		mod_enabled = not mod_enabled
+		GLOBAL.ThePlayer.components.talker:Say("Safe Telepoof: "..(mod_enabled and "on" or "off"))
+	end
+end)
+
 -- This is by far my best practice to override existing componentactions
 -- since COMPONENT_ACTIONS is local variable and modded componentactions are stored in a different namespace.
-local componentactionpoint = UpvalueHacker.GetUpvalue(EntityScript.CollectActions, "COMPONENT_ACTIONS").POINT
-local _blinkstaff = componentactionpoint.blinkstaff
-componentactionpoint.blinkstaff = function(inst, doer, pos, actions, right, target) 
-	if right then 
-		if doer.HUD and doer.components.playercontroller and not doer.components.playercontroller:IsAOETargeting() then
-			return
-		end
-		
-		local aoetargeting = inst.components.aoetargeting
-		if aoetargeting ~= nil and aoetargeting:IsEnabled() then
-			local checker = {} -- instead of passing real actions, pass a fake table instead to check telepoof availability.
-			_blinkstaff(inst, doer, pos, checker, right, target)
-			if #checker > 0 then
-				table.insert(actions, ACTIONS.CASTAOE) -- register CASTAOE instead; what actions inserted is how it interfaces.
+local componentaction_point = UpvalueHacker.GetUpvalue(EntityScript.CollectActions, "COMPONENT_ACTIONS").POINT
+local _blinkstaff = componentaction_point.blinkstaff
+componentaction_point.blinkstaff = function(inst, doer, pos, actions, right, target)
+	if mod_enabled then
+		if right then 
+			if doer.HUD and doer.components.playercontroller and not doer.components.playercontroller:IsAOETargeting() then
+				--@V2C: #FORGE_AOE_RCLICK *searchable*
+				--      -Forge used to strip all r.click actions even before starting aoe targeting,
+				--       so this early out was not needed until now.
+				return 
+			end
+			
+			local aoetargeting = inst.components.aoetargeting
+			local mouseover = TheInput:GetWorldEntityUnderMouse()
+			if aoetargeting ~= nil and aoetargeting:IsEnabled() 
+			and (mouseover == nil or mouseover:HasTag("walkableplatform")) then -- This one has to be added it is not allowed to telepoof on object but CASTAOE has high priority enough to target it.
+				local checker = {} -- instead of passing real actions, pass a fake table instead to check telepoof availability.
+				_blinkstaff(inst, doer, pos, checker, right, target)
+				if #checker > 0 then
+					table.insert(actions, ACTIONS.CASTAOE) -- register CASTAOE instead; what actions inserted is how it interfaces.
+				end
 			end
 		end
+	else 
+		_blinkstaff(inst, doer, pos, actions, right, target)
 	end
 end
 
@@ -39,14 +57,20 @@ AddComponentPostInit("playercontroller", function(self, inst)
 			local invobject = buffaction.invobject
 			local platform, pos_x, pos_z = buffaction.pos.walkable_platform, buffaction.pos.local_pt.x, buffaction.pos.local_pt.z
 			local mouseover = TheInput:GetWorldEntityUnderMouse()
-			local act = BufferedAction(GLOBAL.ThePlayer, nil, ACTIONS.BLINK, invobject, Vector3(pos_x, 0, pos_z))
 			
-			SendRPCToServer(RPC.RightClick, ACTIONS.BLINK.code, pos_x, pos_z, mouseover, nil, nil, nil, nil, nil, platform, platform ~= nil)
-			if not self:CanLocomote() then return end
-			-- TODO: Test this case
-			buffaction = act
+			if self.locomotor == nil then 
+				SendRPCToServer(RPC.RightClick, ACTIONS.BLINK.code, pos_x, pos_z, mouseover, nil, nil, nil, nil, nil, platform, platform ~= nil)
+			elseif self:CanLocomote() and not self:IsBusy() then 
+				-- Lag compensation(movement prediction) is on
+				local act = BufferedAction(GLOBAL.ThePlayer, mouseover, ACTIONS.BLINK, invobject, Vector3(pos_x, 0, pos_z))
+				act.preview_cb = function() 
+					SendRPCToServer(RPC.RightClick, ACTIONS.BLINK.code, pos_x, pos_z, mouseover, nil, nil, nil, nil, nil, platform, platform ~= nil)
+				end
+				self.locomotor:PreviewAction(act, true)
+			end
+		else 
+			_DoAction(self, buffaction, ...)
 		end
-		return _DoAction(self, buffaction, ...)
 	end
 end)
 
